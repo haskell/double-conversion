7c4c885c1022ad7949a5d043af726ae54cc05f9b
Bryan O'Sullivan <bos@mailrank.com>
2011-06-26 19:15:14 -0700

    Add simple QuickCheck tests

---
+++ tests/Properties.hs
@@ -0,0 +1,11 @@
+import Data.Double.Conversion
+import Test.QuickCheck
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import qualified Data.Text as T
+
+shortest a = (read . T.unpack . toShortest) a == a
+
+main = defaultMain [
+        testProperty "shortest" shortest
+       ]

0d8ad82cc8677e8a421d33fbc303b6a68a38a277
Bryan O'Sullivan <bos@serpentine.com>
2011-08-10 14:52:23 -0700

    Cover both Text and ByteString versions of toShortest.

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,11 +1,19 @@
-import Data.Double.Conversion.Text
-import Test.QuickCheck
-import Test.Framework (defaultMain, testGroup)
+import Test.Framework (defaultMain)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Double.Conversion.ByteString as B
+import qualified Data.Double.Conversion.Text as T
 import qualified Data.Text as T
 
-shortest a = (read . T.unpack . toShortest) a == a
+shortest :: (Double -> String) -> Double -> Double -> Bool
+shortest f a b = case read (f ab) of
+                   ba | isNaN ba      -> isNaN ab
+                      | isInfinite ba -> isInfinite ab && signum ba == signum ab
+                      | otherwise     -> ba == ab
+  where ab = a / b
 
+main :: IO ()
 main = defaultMain [
-        testProperty "shortest" shortest
+         testProperty "b_shortest" $ shortest (B.unpack . B.toShortest)
+       , testProperty "t_shortest" $ shortest (T.unpack . T.toShortest)
        ]

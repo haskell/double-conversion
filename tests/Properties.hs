2a30c5fccf3b7c3556d52c9cbacb2b8dbe547094
HaskellMouse <rinat.stryungis@serokell.io>
2022-01-30 01:34:51 +0300

    Fixed tests

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,7 +1,7 @@
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import qualified Data.ByteString.Char8 as B
-import qualified Data.FP.Conversion.Convertable as B
+import qualified Data.Double.Conversion.Convertable as C
 import qualified Data.Text as T
 import qualified Regressions
 
@@ -14,8 +14,8 @@ shortest f a b = case read (f ab) of
 
 tests :: Test
 tests = testGroup "Properties" [
-    testProperty "b_shortest" $ shortest (B.unpack . toShortest)
-  , testProperty "t_shortest" $ shortest (T.unpack . toShortest)
+    testProperty "b_shortest" $ shortest (B.unpack . C.toShortest)
+  , testProperty "t_shortest" $ shortest (T.unpack . C.toShortest)
   ]
 
 main :: IO ()
